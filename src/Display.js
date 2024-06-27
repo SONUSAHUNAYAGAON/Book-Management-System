5568a9857247069a86adeff042128c24081c311a
shivkumar-sahu <shivkuma87r@gmail.com>
2024-06-27 22:33:13 +0530

    create dot env file for API data

--- src/Display.js
+++ src/Display.js
@@ -1,63 +1,50 @@
-import{useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-const Display=()=>
-{
+const Display = () => {
+  const [record, setRecord] = useState([]);
 
-const [record , setRecord]=useState([]);
+  const loaddata = () => {
+    axios
+      .get(`${process.env.REACT_APP_JSON_API}`)
+      .then((res) => setRecord(res.data));
+  };
 
-const loaddata=()=>
-{
- axios.get("http://localhost:5000/Book_Keeping_System")
-   .then((res)=> setRecord(res.data) ); 
-}
+  useEffect(() => {
+    loaddata();
+  }, []);
 
-useEffect(()=>{
-     loaddata();  
-
-}, []);
-
-  return(
+  return (
     <>
-   <center>
-    <h1 class="bg-success text-white container">display record</h1>
-<table class="table">
-       <thead className="bg-primary text-white">
-         <tr class="row ml-2">
-           <th class="col-2">#ID</th>
-           <th class="col-2">Book</th>
-           <th class="col-3">Author</th>
-           <th class="col-2">Publisher</th>
-           <th class="col-2">price</th>
-         </tr>
-       </thead>
-     </table>
+      <center>
+        <h1 class="bg-success text-white container">display record</h1>
+        <table class="table">
+          <thead className="bg-primary text-white">
+            <tr class="row ml-2">
+              <th class="col-2">#ID</th>
+              <th class="col-2">Book</th>
+              <th class="col-3">Author</th>
+              <th class="col-2">Publisher</th>
+              <th class="col-2">price</th>
+            </tr>
+          </thead>
+        </table>
+      </center>
 
- </center>
-   
-
-
-
-    {record.map((key)=>
-    
-   
-     
-
-      <table  class="table">
-       <tbody>
-         <tr className="row ml-2">
-           <td className="col-2">{key.id}</td>
-           <td className="col-2">{key.book}</td>
-           <td className="col-3">{key.author}</td>
-           <td className="col-2">{key.publisher}</td>
-           <td class="col-2">{key.price} </td>
-         </tr>
-        
-       </tbody>
-     </table> )}
-
-
-  </>
-    );
-}
+      {record.map((key) => (
+        <table class="table">
+          <tbody>
+            <tr className="row ml-2">
+              <td className="col-2">{key.id}</td>
+              <td className="col-2">{key.book}</td>
+              <td className="col-3">{key.author}</td>
+              <td className="col-2">{key.publisher}</td>
+              <td class="col-2">{key.price} </td>
+            </tr>
+          </tbody>
+        </table>
+      ))}
+    </>
+  );
+};
 export default Display;
